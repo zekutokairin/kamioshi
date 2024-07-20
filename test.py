6bc7a96ea8313000eac9fecfba75e048e40e7581
Zekū Tōkairin <zeku@heckfire.club>
2024-07-20 14:18:31 -0400

    We are currently getting the Atom feed for a Youtube channel by username.

---
+++ test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import feedparser
+
+from yt_dlp import YoutubeDL
+#import yt_dlp as yt
+#YoutubeDL = yt.YoutubeDL()
+
+YT_BASE_FEEDURL = "https://www.youtube.com/feeds/videos.xml?channel_id="
+
+# Kamioshi Prototype
+# Milestone 1
+# - Parse Youtube channel content as Feed
+# - Download videos in best quality/codec (vp9 or higher)
+# Milestone 2
+# - Download in mobile quality
+# Milestone 3
+# - Using DHT/Kademlia, find peers
+# - Exchange videos 
+# - For Youtube videos, we use the "id" field in the XML e.g. "yt:video:DnCyqDyp-NQ"
+
+"""
+def parseYoutubeFeed(url):
+    if hasattr(ssl, '_create_unverified_context'):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        #xml = 'https://news.google.com/news?q=fashion&output=rss'
+        feed = feedparser.parse(rss)
+"""
+
+def get_channel_id(username):
+    # Get channel ID from provided name/URL
+    with YoutubeDL({'playlist_items':'1'}) as ydl:
+        info = ydl.extract_info("https://www.youtube.com/@zekutokairin", download=False)
+    title = info['title']
+    channel_id = info['channel_id']
+    return channel_id
+
+def addYoutubeChannel(url):
+    # TODO: Parse results and create entries in the DB
+    pass
+
+# TODO: This is for published videos-- can we possibly get Lives as well?
+def parseChannelRSS(channel_id):
+    d = feedparser.parse("https://www.youtube.com/feeds/videos.xml?channel_id=" + channel_id)
+    for entry in d.entries:
+        print("ID: %s\tTitle: %s\tURL:%s" % (entry.id, entry.title, entry.link))
+
+def main():
+    username = "zekutokairin"
+    channel_id = get_channel_id(username)
+
+    parseChannelRSS(channel_id)
+    
+    # https://www.youtube.com/feeds/videos.xml?channel_id=UC2TveK-Kfbp8Wh3PeBh2i6A
+    #addYoutubeChannel("https://www.youtube.com/@zekutokairin")
+
+if __name__ == "__main__":
+    main()
