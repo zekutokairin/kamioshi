5925fe697f1b80be8072a67e30b229db70bd02fb
Zekū Tōkairin <zeku@heckfire.club>
2024-08-07 15:03:16 -0400

    Updates to hopefully get a working copy for 720p.
    If this works, will probably roll out a 1080p version

--- 720fullarchive.sh
+++ 720fullarchive.sh
@@ -31,6 +31,6 @@ yt-dlp --verbose -f "bestvideo[vcodec!~='vp0?9'][height<=?720]+bestaudio/best[he
 
 # Download Livestreams
 #echo "Downloading Livestreams..."
-#yt-dlp --verbose -f "bestvideo[vcodec!~='vp0?9'][height<=?720]+bestaudio/best[height<=?720]" --yes-playlist --write-auto-subs --embed-subs --embed-chapters --embed-thumbnail -o "%(channel)s/Livestreams/%(upload_date)s - %(title)s [%(id)s].%(ext)s" --embed-chapters --download-archive $CHANNEL_NAME.txt $STREAMS_URL
+yt-dlp --verbose -f "bestvideo[vcodec!~='vp0?9'][height<=?720]+bestaudio/best[height<=?720]" --yes-playlist --write-auto-subs --embed-subs --embed-chapters --embed-thumbnail -o "%(channel)s/Livestreams/%(upload_date)s - %(title)s [%(id)s].%(ext)s" --embed-chapters --download-archive $CHANNEL_NAME.txt $STREAMS_URL
 
 
