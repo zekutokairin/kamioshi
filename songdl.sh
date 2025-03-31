c74822cbb722bca2908fb4da3642b2c648c40485
zekutokairin <zeku@heckfire.club>
2025-03-30 23:23:50 -0400

    Updates to get embedded thumbnail and metadata for songs

--- songdl.sh
+++ songdl.sh
@@ -1,1 +1,2 @@
-yt-dlp -f bestaudio -x --audio-format m4a --embed-thumbnail --add-metadata "https://www.youtube.com/watch?v=ZwDHSVYZuTc"
+#yt-dlp -f bestaudio -x --audio-format m4a --embed-thumbnail --add-metadata "https://www.youtube.com/watch?v=ZwDHSVYZuTc"
+yt-dlp -x -f ba --embed-thumbnail --add-metadata $1 
